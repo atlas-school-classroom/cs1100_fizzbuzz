d3bcc1773b2fe349147bda406a8b929abab0d8c7
Bella Bernal <bella.bernal@atlasschool.com>
2025-05-09 14:54:22 +0000

    testing with correct code

--- src/fizz_buzz.c
+++ src/fizz_buzz.c
@@ -2,8 +2,21 @@
 
 void fizz_buzz(int number) {
   /* This function should take in integers and print Fizz, Buzz, or Fizzbuzz based on the number inputed. 
-  
+
   For multiples of 3, print "Fizz" instead of the number. For multiples of 5, print "Buzz". 
   For numbers which are multiples of both 3 and 5, print "FizzBuzz".
   */
+    for (int i = 1; i <= number; i++) {
+        if (i % 3 ==0 && i % 5 ==0) {
+            printf("FizzBuzz\n");
+        }
+        else if (i % 3 == 0) {
+            printf("Fizz\n");
+        }
+        else if (i % 5 == 0) {
+            printf("Buzz\n");
+        } else {
+            printf("%d\n", i);
+        }
+    }
 }
